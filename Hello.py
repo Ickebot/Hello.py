8cbfc3f3faa338ff575220f5f939b9d11e05d8d1
Ickebot <albrechtmaximilian7@gmail.com>
2024-07-19 16:36:54 +0200

    Master_and_student

--- Hello.py
+++ Hello.py
@@ -1,1 +1,4 @@
-print("Hello, old friend")
+print("The master says: Hello, old friend")
+X = "The lost student says:"
+print(f" {X} master...")
+print(f"{X} I have to admit, your face is still as horribal as ever")
